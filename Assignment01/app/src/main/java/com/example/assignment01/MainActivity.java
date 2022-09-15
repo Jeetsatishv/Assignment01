8016ab3d98bf51a57b2c7310367651872ab28200
jeet <jeetv@bu.edu>
2022-09-15 11:14:41 -0400

    CS501 Assignment 1

---
+++ Assignment01/app/src/main/java/com/example/assignment01/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.assignment01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text01;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text01 = findViewById(R.id.textView);
+        text01.setText("??????");
+    }
+
+    public void clickButton1 (View x) {
+        text01.setText("Hello World");
+
+
+
+    }
+}
